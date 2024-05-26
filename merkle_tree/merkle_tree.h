bb966f846e125e9a18f53e29171ebdeab750d735
mauricioLeite <mauricioleite.fe@gmail.com>
2024-05-26 16:18:01 -0300

    feat:  declare GenerateMerkleProof and PrintByLevels methods

--- merkle_tree/merkle_tree.h
+++ merkle_tree/merkle_tree.h
@@ -1,7 +1,10 @@
 #pragma once
 
-#include <vector>
+#include <algorithm>
+#include <cassert>
 #include <string>
+#include <vector>
+
 #include "merkle_node.h"
 
 /**
@@ -13,15 +16,20 @@ class MerkleTree {
 public:
     void FromHashList(std::vector<std::string> leaves);
 
+    std::vector<std::string> GenerateMerkleProof(std::string hash);
+
     void PrintTree();
     void PrintSubTree(MerkleNode *node, uint level);
+    void PrintByLevels();
 
 private:
     std::vector<MerkleNode *> leaves_;
     
     MerkleNode *merkle_root_;
+    std::vector<TreeLevel> tree_levels_;
 
     void GenerateTreeLevels(TreeLevel level);
+    int HashIndexInLevel(std::string hash, TreeLevel level);
     
 };
 
